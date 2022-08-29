6a4c78083625ed78fbf14bf78df9e1e285ab9fb4
Anthony Kim <fbssaejun@gmail.com>
2022-08-29 14:44:32 -0400

    solve coding challenge for converting input object

---
+++ react-redux/redux-starter/src/codingChallenges.js
@@ -0,0 +1,26 @@
+// 1. Write code in a functional style to convert the input to the output:
+input = { tag: "JAVASCRIPT" }
+output = "(javascript)"
+
+
+// Answer 1
+
+function convertInput(obj) {
+  return `(${obj.tag.toLowerCase()})`
+}
+
+convertInput(input) // => "(javascript)"
+
+// 2. We have a recipe object as follows:
+recipe = {
+  name: "Spaghetti Bolognese",
+  ingredients: ["egg", "salt"]
+}
+
+/* Assuming that this object is immutable, write code to
+-Add a new ingredient (“cream”)
+-Replace “egg” with “egg white”
+-Remove an ingredient (“egg”) */
+
+// Answer 2
+
