03377a70f5308deff0936882c639fcd19c524d67
Anthony Kim <fbssaejun@gmail.com>
2022-09-24 16:48:30 -0400

    add bugResolved to reducer, dispatch bugResolved action in index.js

--- react-redux/redux-starter/src/index.js
+++ react-redux/redux-starter/src/index.js
@@ -1,13 +1,12 @@
 import store from './store';
-import { bugAdded, bugRemoved } from './actions';
+import { bugAdded, bugRemoved, bugResolved } from './actions';
+
 //Store gets subscribed
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 })
 
 store.dispatch(bugAdded("bug 1"))
-
-console.log(store.getState())
 /*
 [
   {
@@ -18,12 +17,22 @@ console.log(store.getState())
 ]
 */
 
+store.dispatch(bugResolved(1))
+/*
+[
+  {
+    description: "bug1",
+    id:1,
+    resolved: true
+  }
+]
+*/
+
+
 //Unsubscribe from store logging events
 unsubscribe()
 
 store.dispatch(bugRemoved("bug 1"))
-
-console.log(store.getState())
 /*
 []
 */
