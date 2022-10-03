bd388b6343d027cca3fb4ee016b754a38255c948
Anthony Kim <fbssaejun@gmail.com>
2022-10-02 21:47:03 -0400

    Refactor action creators with createAction function from redux toolkit, clean up unnecessary constants

--- react-redux/redux-starter/src/store/bugs.js
+++ react-redux/redux-starter/src/store/bugs.js
@@ -1,37 +1,16 @@
-// Action types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+import { createAction } from "@reduxjs/toolkit";
 
 // Action creators
-export const bugAdded = description => ({
-  type: BUG_ADDED,
-  payload: {
-    description
-  }
-})
-
-export const bugResolved = id => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id
-  }
-})
-
-export const bugRemoved = description => ({
-  type: BUG_REMOVED,
-  payload: {
-    id: 1,
-    description
-  }
-})
+export const bugAdded = createAction("bugAdded")
+export const bugResolved = createAction("bugResolved")
+export const bugRemoved = createAction("bugRemoved")
 
 // Reducer
 let lastId = 0;
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case BUG_ADDED:
+    case bugAdded.type:
       return [
         ...state,
         {
@@ -40,9 +19,9 @@ export default function reducer(state = [], action) {
           resolved: false
         }
       ];
-    case BUG_REMOVED:
+    case bugRemoved.type:
       return state.filter(bug => bug.id !== action.payload.id)
-    case BUG_RESOLVED:
+    case bugResolved.type:
       return state.map(bug =>
          bug.id !== action.payload.id ? bug : {...bug, resolved: true}
         )
