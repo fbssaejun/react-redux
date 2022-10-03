aab8b2689b13c6ab8d7c64659512fdcc35370b28
Anthony Kim <fbssaejun@gmail.com>
2022-10-02 21:47:29 -0400

    Refactor actions to take in payload object as an argument

--- react-redux/redux-starter/src/index.js
+++ react-redux/redux-starter/src/index.js
@@ -7,7 +7,9 @@ store.subscribe(() => {
   console.log("Store changed!")
 })
 
-store.dispatch(actions.bugAdded("bug 1"))
-store.dispatch(actions.bugAdded("bug 2"))
-store.dispatch(actions.bugAdded("bug 3"))
-store.dispatch(actions.bugResolved(1))
+store.dispatch(actions.bugAdded({ description: "bug 1" }))
+store.dispatch(actions.bugAdded({ description: "bug 2" }))
+store.dispatch(actions.bugAdded({ description: "bug 3" }))
+store.dispatch(actions.bugResolved({ id: 1 }))
+
+console.log(store.getState())
